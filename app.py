1ce1489a088ca0b324fb87a352fb29b0cbbdde87
Aviral Meharishi <aviralmeharishi@gmail.com>
2024-12-17 13:26:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,55 +4,55 @@ import numpy as np
 import pandas as pd
 import pickle
 
-
-with open('final_model_xgb.pkl', 'rb') as file:
+with open('final_model_xgb.pkl','rb') as file:
     model = pickle.load(file)
 
+with open('transformer.pkl','rb') as file:
+        pt = pickle.load(file)
 
-with open('transformer.pkl', 'rb') as file:
-    pt = pickle.load(file)
-
-
+    
 def prediction(input_list):
 
-    input_list = np.array(input_list, dtype = object)
+    tran_data = pt.transform([[input_list[0],input_list[3]]])
+    input_list[0] = tran_data[0][0]
+    input_list[3] = tran_data[0][1]
+
+    input_list = np.array(input_list,dtype=object)
+      
     pred = model.predict_proba([input_list])[:,1][0]
     
-    if pred > 0.5:
-        return f'This Booking is more likely to get canceled : Chances are {round(pred,4)}'
+    if pred>0.5:
+        return f'This booking is more likely to get canceled: Chances {round(pred,2)}'
     else:
-        return f'This Booking is less likely to grt canceled : Chances are {round(pred,4)}'
-
+        return f'This booking is less likely to get canceled: Chances {round(pred,2)}'
+    
 def main():
-    st.title('''INN HOTEL GROUP
-    POWERED BY AVIRAL MEHARISHI CREATIONS ''')
+    st.title('INN HOTEL GROUP')
+    lt = st.text_input('Enter the lead time.')
+    mst = (lambda x:1 if x=='Online' else 0)(st.selectbox('Enter the type of booking',['Online','Offline']))
+    spcl = st.selectbox('Select the no of special requests made',[0,1,2,3,4,5])
+    price = st.text_input('Enter the price offered for the room')
+    adult = st.selectbox('Select the no adults in booking',[0,1,2,3,4])
+    wkd = st.text_input('Enter the weekend nights in the booking')
+    wk = st.text_input('Enter the week nights in booking')
+    park = (lambda x:1 if x=='Yes'else 0)(st.selectbox('Is parking included in the booking',['Yes','No']))
+    month = st.slider('What will be month of arrival',min_value=1,max_value=12,step=1)
+    day = st.slider('What will be day of arrival',min_value=1,max_value=31,step=1)
+    wkday_lambda = (lambda x: 0 if x=='Mon' else 1 if x=='Tue' else 2 if x=='Wed' else 3 if x=='Thus' 
+                    else 4 if x=='Fri' else 5 if x=='Sat' else 6)
+    wkday = wkday_lambda(st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
+
     
-    lt = st.text_input('PLEASE ENTER THE LEAD TIME')
-    mst = (lambda x:1 if x == 'Online' else 0)(st.selectbox('ENTER THE TYPEOF BOOOKING', ['ONLINE', 'OFFLINE']))
-    spcl = st.selectbox('KINDLY ENTER THE NO. OF SPECIAL REQUESTS MADE ',[0,1,2,3,4,5])
-    price = st.text_input('KINDLY ENTER THE PRICE OFFERED FOR THE ROOM')
-    adult = st.selectbox('SELECT THE NUMBER OF ADULTS IN THE BOOKING', [0,1,2,3,4])
-    wkd = st.text_input('PLEASE ENTER THE WEEKEND NIGHTS IN THE BOOKING')
-    wk = st.text_input('PLEASE ENTER THE WEEK NIGHTS IN THE BOOKING')
-    park = (lambda x:1 if x == 'Yes' else 0)(st.selectbox('IS PARKING INCLUDED IN THE BOOKING', ['Yes', 'No']))
-    month = st.slider('WHAT WILL BE THE MONTH OF ARRIVAL ?', min_value = 1, max_value = 12, step = 1)
-    day = st.slider('WHAT WILL BE THE DAY OF ARRIVAL ?', min_value = 1, max_value = 31, step = 1)
-    wkday_lambda = (lambda x:0 if x== 'MON' else 1 if x== 'TUE' else 2 if x== 'WED' else 3 if x== 'THU' 
-                    else 4 if x== 'FRI' else 5 if x== 'SAT' else 6  )
-    wkday = wkday_lambda(st.selectbox('WHAT IS THE WEEKDAY OF ARRIVAL ? ',['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']))
-
-
-
-    tran_data = pt.transform([[lt, price]])
-    lt_t = tran_data[0][0]
-    price_t = tran_data[0][1]
+    inp_list = [lt,mst,spcl,price,adult,wkd,park,wk,month,day,wkday]
     
-    imp_list = [lt_t, mst, spcl, price_t, adult, wkd, wk, park, month, day, wkday]
-
-
-    if st.button('PREDICT'):
+    if st.button('Predict'):
         response = prediction(inp_list)
         st.success(response)
+        
+if __name__=='__main__':
+    main()
+    
+
 
 if __name__ == '__main__':
     main()
